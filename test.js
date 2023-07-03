049aa1f177ede31544431ac9bddadb3561b6f91f
Mauve Signweaver <contact@mauve.moe>
2023-07-03 14:26:33 -0400

    204 response should be number

--- test.js
+++ test.js
@@ -127,7 +127,7 @@ test('HEAD request', async (t) => {
   const headersLastModified = headResponse.headers.get('Last-Modified')
   const headersLink = headResponse.headers.get('Link')
 
-  t.equal(headResponse.status, '204', 'Response had expected status')
+  t.equal(headResponse.status, 204, 'Response had expected status')
   // Version at which the file was added
   t.equal(headersEtag, '2', 'Headers got expected etag')
   t.equal(headersContentType, 'text/plain; charset=utf-8', 'Headers got expected mime type')
